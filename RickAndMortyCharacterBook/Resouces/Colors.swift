e9a99c724e5516a8aaa62f26296731538b4aaedb
MatveyGarbuzov <matveyvoronezh@mail.ru>
2022-04-25 15:31:28 +0300

    Initial Commit

---
+++ RickAndMortyCharacterBook/Resouces/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  sem1
+//
+//  Created by Matvey Garbuzov on 19.04.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let main = UIColor(named: "Main")!
+    static let secondary = UIColor(named: "Secondary")
+    static let greyBG = UIColor(named: "Grey BG")
+    static let BG = UIColor(named: "BG")
+}
